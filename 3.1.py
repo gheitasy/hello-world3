31c3f91df221e04297891b3f64d950c33e574392
gheitasy <32264431+gheitasy@users.noreply.github.com>
2017-10-01 16:30:51 +0100

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,8 @@
+print("This program will add two numbers that you type into it and display the result")
+number1 = int(input("Please enter the first number"))
+print("Thank you")
+number2 = int(input("Now please enter the second number"))
+print("Thank you")
+result = number1+number2
+print("The result is: ")
+print(result)
